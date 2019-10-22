ff348861db2070feaf81d01a48cdcd3feb42f7c5
J <jmrumingan@gmail.com>
2019-10-22 17:32:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+def map(source_array)
+  new_array = source_array 
+  count = 0 
+  while count<new_array.length do 
+    new_array[count] = yield(new_array[count])
+    count += 1 
+  end 
+  return new_array 
+end 
+
+def reduce(source_array, starting_value=0)
+  
+  i = 0 
+  while i<source_array.length do 
+    
+    value = yield(source_array)
+    
+    if value == true 
+      return true 
+    elsif value.class == Integer
+        
+        value += value 
+    end 
+    i+= 1 
+  end 
+  if starting_value != 0 
+    value += starting_value 
+  end 
+  return value 
+end 
